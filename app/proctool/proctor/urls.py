29c50d99b531dd86f73c73717ac8c91889c1bb11
Nizam Yuseri <55922749+starx95@users.noreply.github.com>
2021-02-01 03:45:47 +0800

    image snapshot and mic record

---
+++ app/proctool/proctor/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'proctor'
+
+urlpatterns = [
+   url(r'^violation/$', views.violation, name="violation"),
+   url(r'^test/$', views.test_view, name="test"),
+   url(r'^student-exam/$', views.student_exam, name="student-exam"),
+]
---
+++ app/proctool/proctor/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from users.models import Account
+
+def violation(request):
+    users = Account.objects.all()[1:]
+    print(users[1].violations.fullscreen)
+    context = {
+        'users': users
+    }
+    return render(request, 'proctor/view_all_violations.html', context)
+    
+def test_view(request):
+    context = {}
+    return render(request, 'proctor/test_page.html', context)
+
+def student_exam(request):
+    context = {}
+    return render(request, 'proctor/student_exam_page.html', context)
