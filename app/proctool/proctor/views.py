f2d2c824674b63d2dee84851ae1ab9db9e28afb1
SaurabhAgarwala <saur.agarwala@gmail.com>
2021-02-02 17:24:49 +0530

    Integrate Violations page

--- app/proctool/proctor/views.py
+++ app/proctool/proctor/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from users.models import Account
 
-def violation(request):
+def get_violation(request):
     users = Account.objects.all()[1:]
     print(users[1].violations.fullscreen)
     context = {
         'users': users
     }
-    return render(request, 'proctor/view_all_violations.html', context)
+    return render(request, 'proctor/violations.html', context)
     
+
+@login_required(login_url="/users/login/")
 def test_view(request):
-    context = {}
+    user = request.user
+    context = {'user':user}
     return render(request, 'proctor/test_page.html', context)
+
+
+# def report_violation(request):
+
 
 # def student_exam(request):
 #     context = {}
